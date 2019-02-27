8641689e156cd9f5147bc4e3d0c4fda7c2867451
McNultyPT <mcnultypt@gmail.com>
2019-02-27 15:08:37 -0800

    creating get method to return a student by id

--- students/students-router.js
+++ students/students-router.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+
+    db('students')
+        .where({ id })
+        .then(student => {
+           if (student.length > 0) {
+               res.status(200).json(student);
+           } else {
+               res.status(404).json({ errorMessage: 'A student with that ID does not exist.' });
+           }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The student could not be retrieved.' });
+        });
+});
+
 module.exports = router;
