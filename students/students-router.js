1b390edf0edbee2030a475db42c3aff1bdde1c8e
McNultyPT <mcnultypt@gmail.com>
2019-02-27 15:25:52 -0800

    creating put method to update a student

--- students/students-router.js
+++ students/students-router.js
@@ -72,4 +72,30 @@ router.delete('/:id', (req, res) => {
         });
 });
 
+router.put('/:id', (req, res) => {
+    const studentInfo = req.body;
+    const id = req.params.id;
+
+    if (!studentInfo.name)
+        return res.status(400).json({ errorMessage: 'Please provide a name for the student.' });
+
+    db('students')
+        .where({ id })
+        .update(studentInfo)
+        .then(count => {
+            if (count > 0) {
+                db('students')
+                    .where({ id })
+                    .then(student => {
+                        res.status(200).json(student);
+                    });
+            } else {
+                res.status(404).json({ errorMessage: 'A student with that ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'There was an error while updating the student information.' });
+        });
+});
+
 module.exports = router;
