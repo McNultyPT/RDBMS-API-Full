47164ab2029276ce00b7663e1078c8987986e359
McNultyPT <mcnultypt@gmail.com>
2019-02-27 14:15:13 -0800

    creating get method to return a cohort by id

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -15,4 +15,21 @@ router.get('/', (req,res) => {
         });
 });
 
+router.get('/:id', (req,res) => {
+    const id = req.params.id
+
+    db('cohorts')
+        .where({ id })
+        .then(cohort => {
+            if (cohort.length > 0) {
+                res.status(200).json(cohort);
+            } else {
+                res.status(404).json({ errorMessage: 'A cohort with that ID does not exist.' });
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The cohort could not be retrieved.' });
+        });
+});
+
 module.exports = router;
