5b373bfbd0c59580cb2bbd45c24af2250877345f
McNultyPT <mcnultypt@gmail.com>
2019-02-27 14:12:08 -0800

    creating get request to return list of cohorts

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -5,4 +5,14 @@ const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
 
+router.get('/', (req,res) => {
+    db('cohorts')
+        .then(cohorts => {
+            res.status(200).json(cohorts);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The cohorts could not be retrieved.' });
+        });
+});
+
 module.exports = router;
