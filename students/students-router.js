382f077e5f46ee48e957dc1481b72bb12b23184e
McNultyPT <mcnultypt@gmail.com>
2019-02-27 15:05:41 -0800

    creating a get request to return all students

--- students/students-router.js
+++ students/students-router.js
@@ -5,4 +5,14 @@ const knexConfig = require('../knexfile.js');
 
 const db = knex(knexConfig.development);
 
+router.get('/', (req, res) => {
+    db('students')
+        .then(students => {
+            res.status(200).json(students);
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'The students could not be retrieved.' });
+        });
+});
+
 module.exports = router;
